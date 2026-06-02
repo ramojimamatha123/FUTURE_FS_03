ec4a552554b9f182fb9b2113d216330c7338aecc
ramojimamatha123 <ramojimamatha123@gmail.com>
2026-06-02 19:38:55 +0530

    Update accordion.tsx

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -48,4 +48,4 @@ const AccordionContent = React.forwardRef<
 ));
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent }
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
